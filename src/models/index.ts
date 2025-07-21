bee4cc19f260c20d0f8f54835be6a6a808d9e25d
LHXC <xianyuxwo@qq.com>
2025-07-21 21:18:48 +0800

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -67,14 +67,8 @@ export const oaiModels = z.enum([
 ]);
 
 export const openSourceModels = z.enum([
-  // "meta-llama/Llama-2-7b-hf",
-  "codellama/CodeLlama-7b-hf",
-  "codellama/CodeLlama-70b-hf",
-  "microsoft/phi-2",
-  "google/gemma-7b",
   "deepseek-ai/DeepSeek-R1",
   "Qwen/Qwen2.5-72B",
-  // "mistralai/Mistral-7B-v0.1",
   "tiiuae/falcon-7b",
   "01-ai/Yi-6B",
   "openai/whisper-tiny",
